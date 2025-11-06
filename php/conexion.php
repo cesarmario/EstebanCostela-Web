baf30cbd7d3086207aab69fd9e4d6140c293f387
cesarmario <cesarmariosdelosrios@gmail.com>
2025-11-06 20:09:12 -0300

    Nuevos Requerimientos

---
+++ php/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$hostname_ = "localhost";
+$database_ = "c2281510_costela";
+$username_ = "c2281510_costela";
+$password_ = "VIzo24vela";
+$conexion  = mysqli_connect($hostname_, $username_, $password_, $database_);
+mysqli_set_charset($conexion, 'utf8');
